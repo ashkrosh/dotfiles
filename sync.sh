5585fc3cfdc4f792af1606b290d60974f789d347
Ashkan Roshanayi <ashkan.roshanayi@gmail.com>
2019-09-24 22:34:42 +0200

    sync script between dotfiles and mackup. v0 is ready

--- sync.sh
+++ sync.sh
@@ -1,1 +1,16 @@
-TODO: sync changes in dotfiles to /Users/ashkrosh and run mackup backup at end
+#!/bin/bash
+shopt -s nullglob dotglob     # To include hidden files
+
+for dotfile in "$HOME/.dotfiles/homedir/"*; do
+  if [[ $dotfile == "." || $dotfile == ".." ]]; then
+    continue
+  fi
+
+  f="$HOME/Dropbox/Mackup/${dotfile##*/}"
+  if [[ -f $f ]]; then
+    [[ $dotfile -nt $f ]] && cat $dotfile > $f || cat $f > $dotfile
+  fi 
+
+done
+
+# run mackup backup -f at end....
